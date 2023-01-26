6e8010bd03c1dc151f67341de7ba92b1c7a1868f
Benjamin O <jeep70cp@gmail.com>
2023-01-25 23:16:16 -0500

    make it actually work

--- mobilize.js
+++ mobilize.js
@@ -1,12 +1,23 @@
 function setup() {
   ace.require("ace/lib/net").loadScript("https://rawgithub.com/ajaxorg/ace-builds/master/src-min-noconflict/keybinding-vim.js", 
-  function() { 
-      e = document.querySelector(".ace_editor").env.editor; 
-      e.setKeyboardHandler(ace.require("ace/keyboard/vim").handler); 
-  })
+    function() { 
+      e = document.querySelector(".ace_editor")
+      // this is a hack
+      if (e === null) {
+        setTimeout(() => {
+          setup();
+        }, 500);
+      } else {
+        editor = e.env.editor; 
+        editor.setKeyboardHandler(ace.require("ace/keyboard/vim").handler); 
+        console.log("vim mobilized");
+      }
+    })
 }
 
-window.addEventListener("load", (event) => {
-  console.log("vim mobilized");
-  setup();
-});
+document.readyState === 'loading' ? 
+    document.addEventListener("readystatechange", (event) => {
+      setup();
+    })
+  : 
+    setup();
